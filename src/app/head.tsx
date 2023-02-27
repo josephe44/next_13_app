34856b89cf64b806910a7f89c81a7cbdddf46868
josephe44 <josephe442@gmail.com>
2023-02-27 15:56:34 +0100

    layout components

---
+++ src/app/head.tsx
@@ -0,0 +1,53 @@
+export default function Head() {
+  return (
+    <>
+        <title>Next_13</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Next_13" />
+        <meta name="keywords" content="Next_13" />
+        <meta name="author" content="Next_13" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="google" content="nositelinkssearchbox" />
+        <meta name="google" content="notranslate" />
+        <meta name="referrer" content="origin" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="application-name" content="Next_13" />
+        <meta name="apple-mobile-web-app-title" content="Next_13" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="HandheldFriendly" content="True" />
+        <meta name="MobileOptimized" content="320" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@Next_13" />
+        <meta name="twitter:creator" content="@Next_13" />
+        <meta name="twitter:title" content="Next_13" />
+        <meta name="twitter:description" content="Next_13" />
+        <meta name="twitter:image" content="https://Next_13.com/Next_13.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Next_13" />
+        <meta property="og:title" content="Next_13" />
+        <meta property="og:description" content="Next_13" />
+        <meta property="og:url" content="https://Next_13.com" />
+        <meta property="og:image" content="https://Next_13.com/Next_13.png" />
+        <meta property="og:image:secure_url" content="https://Next_13.com/Next_13.png" />
+        <meta property="og:image:alt" content="Next_13" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="fb:app_id" content="Next_13" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="canonical" href="https://Next_13.com" />
+        
+    </>
+  );
+}
