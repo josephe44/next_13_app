a3c0a8b5ad81f694f9bd49d8fc0010397fece009
josephe44 <josephe442@gmail.com>
2023-02-27 15:37:16 +0100

    route grouping

---
+++ src/app/(marketing)/home/page.tsx
@@ -0,0 +1,7 @@
+export default function MarketingHome() {
+  return (
+    <div>
+      <h1>Hello MarketingHome</h1>
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <div>
+      <form>
+        <input type="text" name="name" id="name" placeholder="enter name" />
+        <input type="email" name="email" id="email" placeholder="enter email" />
+        <button>Send Message</button>
+      </form>
+    </div>
+  );
+}
