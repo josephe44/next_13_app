290acb7ac2f9f967678b62323db71fc484d9b2c5
josephe44 <josephe442@gmail.com>
2023-02-27 16:25:12 +0100

    next navigation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
+
 export default function Contact() {
   return (
     <div>
+      <div>
+        <Link href="/">Home</Link>
+      </div>
+    
+      <div>
+        <Link href={`/blog/peter_obi_voter_result`}>Blog Post</Link>
+      </div>
       <form>
         <input type="text" name="name" id="name" placeholder="enter name" />
         <input type="email" name="email" id="email" placeholder="enter email" />
